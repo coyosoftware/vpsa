cdea8fbdbb16b0349e0b5cf00c065efc5773c305
Gustavo Berdugo <gberdugo@gmail.com>
2015-02-20 11:45:50 -0200

    Mudança dos métodos na api de crediário

--- lib/vpsa/api/installments.rb
+++ lib/vpsa/api/installments.rb
@@ -1,28 +1,16 @@
 module Vpsa
   module Api
     class Installments < Client
-      require_all 'vpsa/searcher/commercial', 'installment_searcher'
       require_all 'vpsa/entity/commercial', 'installment'
       
       base_uri "https://www.vpsa.com.br/apps/api/crediarios"
-      
-      def list(searcher = nil)
-        raise ArgumentError unless searcher.nil? || searcher.is_a?(Vpsa::Searcher::Commercial::InstallmentSearcher)
-        
-        return parse_response(self.class.get("/", :body => build_body(searcher.as_parameter),  :headers => header)) if searcher
-        return parse_response(self.class.get("/", :body => build_body,  :headers => header)) unless searcher
-      end
-      
+          
       def configure(data)
         return parse_response(self.class.post("/", :body => build_body(data), :headers => header))
       end
       
-      def update(id, data)
-        return parse_response(self.class.put("/#{id}", :body => build_body(data), :headers => header))
-      end
-      
-      def find(id)
-        return parse_response(self.class.get("/#{id}", :body => build_body,  :headers => header))
+      def information
+        return parse_response(self.class.get("/", :body => build_body,  :headers => header))
       end
     end
   end
--- spec/vpsa/api/installments_spec.rb
+++ spec/vpsa/api/installments_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 RSpec.describe Vpsa::Api::Installments do
   let(:header) {{"Content-Type" => "application/json", "Accept" => "application/json"}}
   
-  describe "listing" do
+  describe "information" do
     before(:each) do
       stub_request(:get, "https://www.vpsa.com.br/apps/api/crediarios/").to_return(:status => 200)
     end
@@ -11,11 +11,7 @@ RSpec.describe Vpsa::Api::Installments do
     it "should issue a get to the installments url" do
       expect(Vpsa::Api::Installments).to receive(:get).with("/", :body => {:token => "abc"}.to_json, :headers => header).and_call_original
       
-      Vpsa.new("abc").installments.list
-    end
-    
-    it "should raise ArgumentError if the parameter is not a InstallmentSearcher" do
-      expect{Vpsa.new("abc").installments.list(Array.new)}.to raise_error(ArgumentError)
+      Vpsa.new("abc").installments.information
     end
   end
   
@@ -30,32 +26,6 @@ RSpec.describe Vpsa::Api::Installments do
       expect(Vpsa::Api::Installments).to receive(:post).with("/", :body => installment_param.merge!({:token => "abc"}).to_json, :headers => header).and_call_original
       
       Vpsa.new("abc").installments.configure(installment_param)
-    end
-  end
-  
-  describe "updating" do
-    before(:each) do
-      stub_request(:put, "https://www.vpsa.com.br/apps/api/crediarios/1").to_return(:status => 200)
-    end
-    
-    let(:installment_param) {{}}
-    
-    it "should issue a put to the installments url" do
-      expect(Vpsa::Api::Installments).to receive(:put).with("/1", :body => installment_param.merge!({:token => "abc"}).to_json, :headers => header).and_call_original
-      
-      Vpsa.new("abc").installments.update(1, installment_param)
-    end
-  end
-  
-  describe "finding" do
-    before(:each) do
-      stub_request(:get, "https://www.vpsa.com.br/apps/api/crediarios/5").to_return(:status => 200)
-    end
-    
-    it "should issue a get to the installment url" do
-      expect(Vpsa::Api::Installments).to receive(:get).with("/5", :body => {:token => "abc"}.to_json, :headers => header).and_call_original
-      
-      Vpsa.new("abc").installments.find(5)
     end
   end
 end
