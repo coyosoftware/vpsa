0262f10e822b11b6ff2583897beea9ca05dd5605
candrade <cesara.deandrade@gmail.com>
2015-11-23 14:43:02 -0200

    ROX-77 - Modificar o servidor de API do ERP Varejonline

--- lib/vpsa.rb
+++ lib/vpsa.rb
@@ -4,8 +4,7 @@ module Vpsa
   LIBNAME = 'vpsa'
   LIBDIR = File.expand_path("../#{LIBNAME}", __FILE__)
 
-  API_ADDRESS = "https://www.vpsa.com.br/apps/api"
-  #API_ADDRESS = "http://192.168.1.204:8080/vpsa/api"
+  API_ADDRESS = "https://integrador.varejonline.com.br/apps/api"
   
   class << self
     def included(base)
